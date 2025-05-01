582aead3cc57d24c6ce02984aef383255bb17221
Niloy Saha <sahaniloy389@gmail.com>
2025-05-01 00:46:44 -0400

    Update content.js

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -2,7 +2,7 @@
   const bodyText = document.body.innerText;
   const videoLink = window.location.href;
 
-  const response = await fetch("https://ai-summarizer-chrome-extensionnn.onrender.com//summarize", {
+  const response = await fetch("http://127.0.0.1:5000/summarize", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
