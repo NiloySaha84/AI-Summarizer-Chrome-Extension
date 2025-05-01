7ee23fb185e63ff0cf5df66d6e54725c2984bfc8
Niloy Saha <sahaniloy389@gmail.com>
2025-05-01 00:45:14 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,6 @@ from flask_cors import CORS
 from website import create_app
 import os
 app = create_app()
-CORS(app, origins=["chrome-extension://<kiaepdpkodigeeonibkmlkkbglomgigd>"])
+CORS(app)
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
